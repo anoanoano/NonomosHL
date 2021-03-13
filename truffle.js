50e21f79b9e93f71c2a02583dc71d62ace150666
prewitt <mtprewitt@gmail.com>
2021-03-12 20:00:59 -0800

    gas price

--- truffle.js
+++ truffle.js
@@ -34,7 +34,8 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/"+infura_key)
       },
       network_id: 1,
-      gas: 6721975
+      gas: 6721975,
+      gasPrice: 150000000000
     },
 
     ropsten: {
